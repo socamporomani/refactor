96d8c0aba5c182b44db4b07db3ab206bbe29865b
socamporomani <socamporomani@nautilus10.danielcastelao.org>
2018-10-23 10:00:48 +0200

    nada

---
+++ boletin4_2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_2;
+
+/**
+ *
+ * @author socamporomani
+ */
+public class boletin4_2 {
+    
+        public static void main(String[] args) {
+        Satelite comprobar= new Satelite();
+        comprobar.verPosicion();
+   
+        }
+}
